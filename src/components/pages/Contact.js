ec50224f62de52d5a42eb1903b15bd2a0e5192e7
marcCodez <marc_pulumbarit@hotmail.com>
2021-02-16 20:16:14 +1100

    contact grid

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -11,8 +11,10 @@ function Contact() {
                             <input type='text' className='name-input' name='name' placeholder='Full Name'/>
                             <input type='email' className='email-input' name='email' placeholder='Email'/>
                             <input type='text' className='subject-input' name='subject' placeholder='Subject'/>
+                            <input type='text' className='phone-input' name='phone' placeholder='Phone Number'/>
                             <input type='text' className='message-input' name='message' placeholder='Message'/>
                     </div>
+                    <button type="submit">Submit</button>
                     </form>
             </div>
         </section>
