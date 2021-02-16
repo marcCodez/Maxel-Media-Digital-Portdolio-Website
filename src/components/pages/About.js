f6412b4657e92223fdaa5a6fd1ad89869b64e9db
marcCodez <marc_pulumbarit@hotmail.com>
2021-02-16 19:30:59 +1100

    contact form progress

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ function About() {
     return (
         <section id='about' class='text-center py-3'>
             <div class='container about-info'>
-            <h1 className="heading">ABOUT ME</h1>
+            <h1 className="heading">ABOUT MAXEL MEDIA</h1>
             <img src='/images/maxpic.jpg' className='about-image'></img>
             <p className='about-description'>Max Lerion is a content creator based in Sydney, Australia. Under an alias of Maxel Media he has been creating content for over 2 years and has recently started to publish client work on social media less than a year ago. He has worked with numerous brands, businesses and artists in  marketing with his diverse skills in photography and videography. Currently in his final year in a bachelor of Media (Screen and Sound Production), he aims to build his brand as a freelancer into a production company to help cater to businesses around Sydney in digital marketing and to bring visions to reality.</p>
             </div>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,15 +3,19 @@ import '../../App.scss'
 
 function Contact() {
     return (
-        <div>
-            <h1>contact</h1>
-            <form>
-               <input type='text' name='name' placeholder='Full Name'/>
-               <input type='email' name='email' placeholder='Email'/>
-               <input type='text' name='subject' placeholder='Subject'/>
-               <input type='text' name='message' placeholder='Message'/>
-            </form>
-        </div>
+        <section id='contact' class='text-center py-3'>
+            <div className='container'>
+                <h1>CONTACT MAXEL MEDIA</h1>
+                    <form method="POST">
+                        <div className='text-fields'>
+                            <input type='text' className='name-input' name='name' placeholder='Full Name'/>
+                            <input type='email' className='email-input' name='email' placeholder='Email'/>
+                            <input type='text' className='subject-input' name='subject' placeholder='Subject'/>
+                            <input type='text' className='message-input' name='message' placeholder='Message'/>
+                    </div>
+                    </form>
+            </div>
+        </section>
     )
 }
 
