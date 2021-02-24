251a91b0fef669d05b1269b59acf763f6fdf7705
marcCodez <marc_pulumbarit@hotmail.com>
2021-02-24 16:27:14 +1100

    netlify contact form submission

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,13 +8,14 @@ function Contact() {
                 <h1 className='section-title'>CONTACT MAXEL MEDIA</h1>
                 <div className='underline'></div>
                 <p className='sub-text'>Alternatively PM on Instagram or Facebook</p>
-                    <form method="POST">
+                    <form action="POST" data-netlify="true">
                         <div className='text-fields'>
                             <input type='text' className='text-input name-input' name='name' placeholder='Full Name'/>
                             <input type='email' className='text-input email-input' name='email' placeholder='Email'/>
                             <input type='text' className='text-input subject-input' name='subject' placeholder='Subject'/>
                             <input type='text' className='text-input phone-input' name='phone' placeholder='Phone Number'/>
                             <textarea type='text' className='text-input message-input' name='message' placeholder='Message'></textarea>
+                            <div data-netlify-recaptcha='true'></div>
                     </div>
                     <button type="submit" className='btn-dark'>Submit</button>
                     </form>
