f7eca2014854aa6cc16bbe51f6aa4d80b4a565f0
marcCodez <marc_pulumbarit@hotmail.com>
2021-02-24 17:12:53 +1100

    contact form issue fix

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,15 +8,16 @@ function Contact() {
                 <h1 className='section-title'>CONTACT MAXEL MEDIA</h1>
                 <div className='underline'></div>
                 <p className='sub-text'>Alternatively PM on Instagram or Facebook</p>
-                    <form action="POST" data-netlify="true">
+                    <form name='contact' method="POST" data-netlify="true">
                         <div className='text-fields'>
                             <input type='text' className='text-input name-input' name='name' placeholder='Full Name'/>
                             <input type='email' className='text-input email-input' name='email' placeholder='Email'/>
                             <input type='text' className='text-input subject-input' name='subject' placeholder='Subject'/>
                             <input type='text' className='text-input phone-input' name='phone' placeholder='Phone Number'/>
                             <textarea type='text' className='text-input message-input' name='message' placeholder='Message'></textarea>
-                            <div data-netlify-recaptcha='true'></div>
+                           
                     </div>
+                    <div data-netlify-recaptcha='true'></div>
                     <button type="submit" className='btn-dark'>Submit</button>
                     </form>
             </div>
