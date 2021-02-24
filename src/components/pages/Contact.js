eefc77ceda0b116ae81cccd34c4ac36f28445aa4
marcCodez <marc_pulumbarit@hotmail.com>
2021-02-24 22:14:31 +1100

    recaptcha retest

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,14 +8,14 @@ function Contact() {
                 <h1 className='section-title'>CONTACT MAXEL MEDIA</h1>
                 <div className='underline'></div>
                 <p className='sub-text'>Alternatively PM on Instagram or Facebook</p>
-                    <form name='contact' method='POST' data-netlify='true' >
+                    <form name='contact' method='POST' data-netlify-recaptcha='true' data-netlify='true' >
                         <input type='hidden' name='form-name' value='contact' />
                         <div className='text-fields'>
-                            <input type='text' className='text-input name-input' name='name' placeholder='Full Name'/>
-                            <input type='email' className='text-input email-input' name='email' placeholder='Email'/>
-                            <input type='text' className='text-input subject-input' name='subject' placeholder='Subject'/>
-                            <input type='text' className='text-input phone-input' name='phone' placeholder='Phone Number'/>
-                            <textarea type='text' className='text-input message-input' name='message' placeholder='Message'></textarea>
+                            <input required type='text' className='text-input name-input' name='name' placeholder='Full Name'/>
+                            <input required type='email' className='text-input email-input' name='email' placeholder='Email'/>
+                            <input required type='text' className='text-input subject-input' name='subject' placeholder='Subject'/>
+                            <input type='text' className='text-input phone-input' name='phone' placeholder='Phone Number (Optional)'/>
+                            <textarea required type='text' className='text-input message-input' name='message' placeholder='Message'></textarea>
                            
                     </div>
                     <div data-netlify-recaptcha='true'></div>
