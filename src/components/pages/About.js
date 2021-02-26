270b0964846d98e91bbafd0f70e61ff30c038bac
marcCodez <marc_pulumbarit@hotmail.com>
2021-02-26 15:15:02 +1100

    fix line spacing, add videos, remove sub text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,7 +9,6 @@ function About() {
                 <div className='heading'>
             <h1 className='section-title'>ABOUT MAXEL MEDIA</h1>
             <div className='underline'></div>
-            <p className='sub-text'>Get to know me</p>
             </div>
             <img src='/images/maxpic.jpg' className='about-image'/>
             <p className='about-description'>Max Lerion is a content creator based in Sydney, Australia. Under an alias of Maxel Media he has been creating content for over 2 years and has recently started to publish client work on social media less than a year ago. He has worked with numerous brands, businesses and artists in  marketing with his diverse skills in photography and videography. Currently in his final year in a bachelor of Media (Screen and Sound Production), he aims to build his brand as a freelancer into a production company to help cater to businesses around Sydney in digital marketing and to bring visions to reality.</p>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -11,14 +11,17 @@ function Projects() {
                 <div className='container'>
                     <h2 className='section-title'>My Work</h2>
                     <div className='underline'></div>
-                    <p className='sub-text'>Check out some of my projects</p>
              
                 
                     <div className='projects'>
+                    <div className='project'>
+                             <ProjectVideo  url='https://youtu.be/8Dk2215smvw'/> 
+                        </div> 
+                    <div className='project'>
+                            <ProjectVideo  url='https://youtu.be/971yh19K23k'/>
+                    </div>
                         <div className='project'>
-                             <ProjectVideo  url='http://www.youtube.com/watch?v=Z12UvF82BJ0&t=0m25s'/>
-                             
-  
+                             <ProjectVideo  url='http://www.youtube.com/watch?v=Z12UvF82BJ0&t'/> 
                         </div> 
                         <div className='project'>
                             <ProjectVideo  url='https://youtu.be/1zmp8eVvavg'/>
