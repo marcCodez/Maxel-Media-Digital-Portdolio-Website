d459534a791d8d3bd716e2d171a18c384d7d8dbe
marcCodez <marc_pulumbarit@hotmail.com>
2021-02-26 23:17:55 +1100

    added video play, pause icons

--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useRef} from 'react'
 import { Link } from 'react-router-dom';
 import '../App.scss';
 
 function HeaderContent() {
+
+
+const vidRef = useRef(null);
+const playVideo = () => {
+    vidRef.current.play()
+}
+
+const pauseVideo = () => {
+    vidRef.current.pause()
+}
+
     return (
         <div className='header-container space-top'>
-            <video src="/videos/Showreel-cut-compressed.mp4" autoPlay loop muted controls/>
+            <video ref={vidRef} src="/videos/Showreel-cut-compressed.mp4" autoPlay loop muted controls/>
            <div className='hero-btns'>
                <Link
                to='/contact'
@@ -16,6 +27,21 @@ function HeaderContent() {
                     Let's Collab!
                 </button>
                 </Link>
+                <div className='vid-controls'>
+                <a 
+                    onClick={playVideo}
+                    aria-label='Play-button'
+                    >
+                       <i class="fa fa-play-circle play-button"></i>
+                </a>
+
+                <a 
+                    onClick={pauseVideo}
+                    aria-label='Play-button'
+                    >
+                       <i class="fa fa-pause-circle pause-button"></i>
+                </a>
+                </div>
            </div>
 
         </div>
