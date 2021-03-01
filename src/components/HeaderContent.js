4973fe23fedfbd9db8d3074a4d76112d6ae61fb5
marcCodez <marc_pulumbarit@hotmail.com>
2021-03-01 18:11:56 +1100

    andriod fix 3

--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -17,7 +17,7 @@ const pauseVideo = () => {
 
     return (
         <div className='header-container space-top'>
-            <video collapsable={ false } ref={vidRef} src="/videos/Showreel-cut-compressed.mp4" autoPlay loop muted/>
+            <video ref={vidRef} src="/videos/Showreel-cut-compressed.mp4" autoPlay loop muted/>
            <div className='hero-btns'>
                <Link
                to='/contact'
