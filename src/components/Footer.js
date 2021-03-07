96907b2573f82cc0ab72757adee14967c8e67e3b
marcCodez <marc_pulumbarit@hotmail.com>
2021-03-07 15:50:37 +1100

    copyright fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ function Footer() {
                  <i className='fa fa-youtube'/>
                  </a>
                  </div>
-                 <p className="copyright">© Marc Pulumbarit 2021</p>
+                 <p className="copyright">© Marc 2021</p>
         </footer>
     )
 }
