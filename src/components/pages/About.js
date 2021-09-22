831dccb18113b83b0786ec165d563fdbf564c3b6
marcpulz <marc_pulumbarit@hotmail.com>
2021-09-22 18:29:09 +1000

    added letter spacing, increase font size and added animations

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import '../../App.scss'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function About() {
+    useEffect(() => {
+        AOS.init({ 
+            duration: 1500
+        });
+    }, [])
     return (
         <section id='about' className='text-center pb-3 space-top'>
             <div className='container'>
                 <div className='about-info'>
                 <div className='heading'>
-            <h1 className='section-title font-weight-bold'>ABOUT MAXEL MEDIA</h1>
-            <div className='underline'></div>
+            <h1 data-aos="fade-left" data-aos-delay="250" className='section-title font-weight-bold'>ABOUT MAXEL MEDIA</h1>
+            <div data-aos="fade-left" data-aos-delay="250" className='underline'></div>
             </div>
-            <img src='/images/maxpic.jpg' className='about-image' alt="Maxel Media in action holding a camera"/>
-            <p className='about-description'>Max Lerion is a content creator based in Sydney, Australia. Under an alias of Maxel Media he has been creating content for over 2 years and has recently started to publish client work on social media less than a year ago. He has worked with numerous brands, businesses and artists in  marketing with his diverse skills in photography and videography. Currently in his final year in a bachelor of Media (Screen and Sound Production), he aims to build his brand as a freelancer into a production company to help cater to businesses around Sydney in digital marketing and to bring visions to reality.</p>
+            <img data-aos="zoom-in" src='/images/maxpic-min.jpg' className='about-image' alt="Maxel Media in action holding a camera"/>
+            <p data-aos="fade-left" data-aos-delay="250" className='about-description'>Max Lerion is a content creator based in Sydney, Australia. Under an alias of Maxel Media he has been creating content for over 2 years and has recently started to publish client work on social media less than a year ago. He has worked with numerous brands, businesses and artists in  marketing with his diverse skills in photography and videography. Currently in his final year in a bachelor of Media (Screen and Sound Production), he aims to build his brand as a freelancer into a production company to help cater to businesses around Sydney in digital marketing and to bring visions to reality.</p>
             </div>
             </div>
         </section>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,64 +1,71 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import '../../App.scss'
 import ReactPlayer from 'react-player'
 import ProjectVideo from '../ProjectVideo'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 function Projects() {
+    useEffect(() => {
+        AOS.init({ 
+            duration: 1500
+        });
+    }, [])
     return (
         <>
             <section class="text-center pb-3 space-top">
                 <div className='container'>
-                    <h2 className='section-title'>MY WORK</h2>
-                    <div className='underline'></div>
+                    <h2 data-aos="zoom-in" className='section-title'>MY WORK</h2>
+                    <div data-aos="zoom-in" className='underline'></div>
              
                 
                     <div className='projects'>
-                    <div className='project'>
+                    <div data-aos="fade-up" data-aos-delay="150" className='project'>
                              <ProjectVideo  url='https://youtu.be/8tsU01FNO8I' title="Iron Gym Platinum"/> 
                         </div> 
-                    <div className='project'>
+                    <div data-aos="fade-up" className='project'>
                              <ProjectVideo  url='https://youtu.be/qezBKmfDa3Y' title="SoulXpress Usyd promo video"/> 
 
                         </div> 
 
-                    <div className='project'>
+                    <div data-aos="fade-up" className='project'>
                              <ProjectVideo  url='https://youtu.be/8Dk2215smvw' title="Stop music video by Tk"/> 
                         </div> 
-                    <div className='project'>
+                    <div data-aos="fade-up" className='project'>
                             <ProjectVideo  url='https://youtu.be/971yh19K23k' title="UNSW MODSOC 2021 PROMO VIDEO"/>
                     </div>
-                        <div className='project'>
+                        <div data-aos="fade-up" className='project'>
                              <ProjectVideo  url='http://www.youtube.com/watch?v=Z12UvF82BJ0&t' title="Dante 20 music video"/> 
                         </div> 
-                        <div className='project'>
+                        <div data-aos="fade-up" className='project'>
                             <ProjectVideo  url='https://youtu.be/1zmp8eVvavg' title="Share With Oscar uni project video" />
                         </div>
-                        <div className='project'>
+                        <div data-aos="fade-up" className='project'>
                             <ProjectVideo  url='https://www.youtube.com/watch?v=PbMBsEsRlrE' title="dante thursday music video"/>
                         </div>   
-                        <div className='project'>
+                        <div data-aos="fade-up" className='project'>
                             <ProjectVideo  url='https://youtu.be/ibY-ZKUVvyA' title="Steps to Problem Solving: An Engineer's Guide - Book Teaser"/>
                         </div>   
-                        <div className='project'>
+                        <div data-aos="fade-up" className='project'>
                             <ProjectVideo  url='https://youtu.be/pZYnPs9caO8' title="Steps to Problem Solving: An Engineer's Guide - Official Book Trailer"/>
                         </div>   
-                        <div className='project'>
+                        <div data-aos="fade-up" className='project'>
                             <ProjectVideo  url='https://www.youtube.com/watch?v=zKwkc92ubGA' title="Robin's Breaking (Beginners Class)"/>
                         </div>   
-                        <div className='project'>
+                        <div data-aos="fade-up" className='project'>
                             <ProjectVideo  url='https://www.youtube.com/watch?v=9X08-17Uoyc' title="Lionel's Girl Style (Beginners Class)"/>
                         </div>   
-                        <div className='project'>
+                        <div data-aos="fade-up" className='project'>
                             <ProjectVideo  url='https://youtu.be/s1Ht_11s-1k' title="Fashion Shoot"/>
                         </div>  
-                        <div className='project'>
+                        <div data-aos="fade-up" className='project'>
                             <ProjectVideo  url='https://www.youtube.com/watch?v=rSlDqYWwKlE' title="Barber in 60"/>
                         </div>  
-                        <div className='project'>
+                        <div data-aos="fade-up" className='project'>
                             <ProjectVideo  url='https://www.youtube.com/watch?v=MiVtLIN__1c' title="Sesame Street Broadway"/>
                         </div>  
-                        <div className='project'>
+                        <div data-aos="fade-up" className='project'>
                             <ProjectVideo  url='https://www.youtube.com/watch?v=O9E1153wZx4' title="101KISSA"/>
                         </div>   
 
