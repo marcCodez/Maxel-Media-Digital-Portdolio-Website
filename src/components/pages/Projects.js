0782be9290ea184f9de8019e93e8fd9eec227eec
marcpulz <marc_pulumbarit@hotmail.com>
2022-06-26 16:22:19 +1000

    projects add pt 3 22

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -24,7 +24,10 @@ function Projects() {
                     <div data-aos="fade-up" className='project'>
                              <ProjectVideo  url='https://youtu.be/XvJvJMaL6X8' title="CLUB REELCOMPILATIONS"/> 
                         </div>
-                    <div data-aos="fade-up" className='project'>
+                        <div data-aos="fade-up" className='project'>
+                             <ProjectVideo  url='https://www.dropbox.com/s/sq40c2jr5fnwx4w/CRUISE_PROMO.mp4?dl=0' title="Cruise promo"/> 
+                        </div>
+                        <div data-aos="fade-up" className='project'>
                              <ProjectVideo  url='https://youtube.com/shorts/0dLg8FrAQ5Y' title="dj promo"/> 
                         </div>
                     <div data-aos="fade-up" className='project'>
